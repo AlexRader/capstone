3741526e0608922c04e21661553d72932f6ceb4b
Alexander Rader <arader@comcast.net>
2018-09-28 14:20:25 -0400

    overflow issue will need to address

---
+++ TestFolder/LightingTest/Assets/Scripts/PlayerMovement1.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement1 : MonoBehaviour
+{
+
+    private Rigidbody myRigidbody;
+    public float moveSpeed; //Leave this public unless you need it not to be so I can change it in editor - Your Pal, Josh. Or I guess you could just serialize the field or w/e
+    public Vector3 subSpeed;
+    float horizontal, vertical;
+    bool lockIn;
+    GameObject target;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        myRigidbody = GetComponent<Rigidbody>();
+        subSpeed = Vector3.zero;
+        lockIn = false;
+        target = GameObject.FindGameObjectWithTag("sub");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //float horizontal = Input.GetAxis("Horizontal"); //Uses Unity Input Manager settings for WASD and Arrows
+
+        //HandleMovement(horizontal);
+    }
+
+    void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.J))
+        {
+            horizontal = -1;
+        }
+        else if (Input.GetKey(KeyCode.L))
+        {
+            horizontal = 1;
+        }
+        if (Input.GetKey(KeyCode.I))
+        {
+            vertical = 1;
+        }
+        else if (Input.GetKey(KeyCode.K))
+        {
+            vertical = -1;
+        }
+        //= Input.GetAxis("Horizontal"); //Uses Unity Input Manager settings for WASD and Arrows // Using Fixed Update for this so that movement speed isn't bound by Framerate
+
+        if (!lockIn)
+            HandleMovement(horizontal);
+        else
+            target.SendMessage("SetJoy", new Vector3(horizontal, 0, vertical));
+        horizontal = 0;
+    }
+
+    private void HandleMovement(float horizontal)//So I could use horizontal inputs for movement, I think this works for joystick too, I will test it with ps4 controller
+    {
+
+        myRigidbody.velocity = new Vector3(horizontal * moveSpeed, 0, myRigidbody.velocity.y) + subSpeed; //x = -1 or 1  y = 0 No vertical movement.
+    }
+    private void OnTriggerStay(Collider collision)
+    {
+        if (collision.tag == "Control")
+        {
+            if (Input.GetKeyDown(KeyCode.N))
+            {
+                lockIn = !lockIn;
+            }
+        }
+    }
+}
---
+++ TestFolder/LightingTest/Assets/Scripts/SearchLight.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SearchLight : MonoBehaviour {
+
+    public float curYRotation;
+	public float minYRotation;
+	public float maxYRotation;
+
+	public float increaseAngle;
+	public float decreaseAngle;
+
+
+    public bool playerInRange, playerInRange1;
+    //public float zRotation = 10.0F;
+   
+    // Use this for initialization
+    void Start () {
+
+        //transform.eulerAngles = new Vector3(0, 0, curZRotation);
+        // transform.rotation = Quaternion.Euler(0, 0, 0);
+        playerInRange = false;
+        playerInRange1 = false;
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+
+        lockedRotation();
+	//	curZRotation = (curZRotation > 180) ? curZRotation - 360 : curZRotation;
+
+
+    }
+
+    private void FixedUpdate()
+    {
+        if (playerInRange == true)
+        {
+            if (Input.GetKey(KeyCode.Q))
+            {
+				curYRotation = curYRotation + increaseAngle;
+            }
+
+            if (Input.GetKey(KeyCode.E))
+            {
+				curYRotation = curYRotation + decreaseAngle;
+            }
+        }
+        if (playerInRange1 == true)
+        {
+            if (Input.GetKey(KeyCode.U))
+            {
+                curYRotation = curYRotation + increaseAngle;
+            }
+
+            if (Input.GetKey(KeyCode.O))
+            {
+                curYRotation = curYRotation + decreaseAngle;
+            }
+        }
+    }
+
+    void lockedRotation()
+    {
+		curYRotation = Mathf.Clamp(curYRotation, minYRotation, maxYRotation);
+        transform.localEulerAngles = new Vector3(transform.localEulerAngles.x, -curYRotation, transform.localEulerAngles.z);
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.tag == "Player" && collision.name == "TestPlayer")
+        {
+           // Debug.Log("In LightBox");
+            playerInRange = true;
+        }
+        else if (collision.tag == "Player" && collision.name == "TestPlayer1")
+        {
+            // Debug.Log("In LightBox");
+            playerInRange1 = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider collision)
+    {
+        if (collision.tag == "Player" && collision.name == "TestPlayer")
+        {
+            //Debug.Log("Exiting LightBox");
+            playerInRange = false;
+        }
+        if (collision.tag == "Player" && collision.name == "TestPlayer1")
+        {
+            //Debug.Log("Exiting LightBox");
+            playerInRange1 = false;
+        }
+    }
+
+}
