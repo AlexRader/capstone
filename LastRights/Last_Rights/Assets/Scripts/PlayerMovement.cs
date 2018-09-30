c24aac6d3996ef35b852e9bea317e5d4eef913e4
Alexander Rader <arader@comcast.net>
2018-09-30 14:44:48 -0400

    setting up movement scripts

---
+++ LastRights/Last_Rights/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : playerOrder
+{
+    private InputNames  inputs;
+    public int          playerNum;
+    float               horizontal,
+                        vertical;
+    bool                lb,
+                        rb,
+                        lt,
+                        rt;
+    public PlayerMovement()
+    {
+        inputs = returnDefaultInputs();
+        setInputs(playerNum);
+    }
+
+
+    // Use this for initialization
+    void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        getInputs();
+        Movement();
+	}
+
+    void setInputs(int var)
+    {
+        inputs.horizontalMove   += var.ToString();
+        inputs.verticalMove     += var.ToString();
+        inputs.leftBumper       += var.ToString();
+        inputs.rightBumper      += var.ToString();
+        inputs.leftTrigger      += var.ToString();
+        inputs.rightTrigger     += var.ToString();
+    }
+
+    void getInputs()
+    {
+        horizontal = Input.GetAxis(inputs.horizontalMove);
+        vertical   = Input.GetAxis(inputs.verticalMove);
+        lb         = Input.GetButton(inputs.leftBumper);
+        rb         = Input.GetButton(inputs.rightBumper);
+        lt         = Input.GetButton(inputs.leftTrigger);
+        rt         = Input.GetButton(inputs.rightTrigger);
+    }
+
+    void Movement()
+    {
+
+    }
+}
---
+++ LastRights/Last_Rights/Assets/Scripts/playerOrder.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerOrder : MonoBehaviour
+{
+    private InputNames playerInputs;
+
+    public playerOrder()
+    {
+        playerInputs.horizontalMove = "horMove";
+        playerInputs.verticalMove   = "vertMove";
+        playerInputs.leftBumper     = "L1";
+        playerInputs.rightBumper    = "R1";
+        playerInputs.leftTrigger    = "L2";
+        playerInputs.rightTrigger   = "R2";
+    }
+
+    public struct InputNames
+    {
+        public string   horizontalMove,
+                        verticalMove,
+                        leftBumper,
+                        rightBumper,
+                        leftTrigger,
+                        rightTrigger;
+
+    };
+
+    public InputNames returnDefaultInputs()
+    {
+        return playerInputs;
+    }
+}
