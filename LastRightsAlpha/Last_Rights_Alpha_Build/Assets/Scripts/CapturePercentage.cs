738b893ad7764c49ca11044ae85e38f16e0f1341
AlexRader <arader@comcast.net>
2018-11-04 22:58:50 -0500

    almost done with full game loop

---
+++ LastRightsAlpha/Last_Rights_Alpha_Build/Assets/Scripts/CapturePercentage.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CapturePercentage : MonoBehaviour
+{
+    public List<GameObject> myTargets;
+    int team1C, team2C;
+    int converter;
+    public float percentage1, percentage2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        team1C = team2C = 0;
+        percentage1 = percentage2 = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CapturePercentageUpdate();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player" && collision.isTrigger)
+        {
+            myTargets.Add(collision.gameObject);
+        }
+    }
+
+    void CapturePercentageUpdate()
+    {
+        if (myTargets.Count > 0)
+        {
+            foreach(GameObject plr in myTargets)
+            {
+                if (plr.GetComponent<Damage>().team == 1)
+                    ++team1C;
+                else
+                    ++team2C;
+            }
+            if (team1C > 0 && team2C > 0)
+            { }
+            else if (team1C > 0)
+            {
+                percentage1 += Time.deltaTime;
+            }
+            else
+            {
+                percentage2 += Time.deltaTime;
+            }
+
+        }
+    }
+}
---
+++ LastRightsAlpha/Last_Rights_Alpha_Build/Assets/Scripts/finderScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+public class finderScript : MonoBehaviour
+{
+    GameObject Sarcaphogas;
+    public TextMeshProUGUI team1, team2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Sarcaphogas = GameObject.FindGameObjectWithTag("MainCapture");
+        Debug.Log(Sarcaphogas.name);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        team1.text = "Team 1 %: " + Sarcaphogas.GetComponent<CapturePercentage>().percentage1;
+        team2.text = "Team 2 %: " + Sarcaphogas.GetComponent<CapturePercentage>().percentage2;
+    }
+}
